5fb43137c07efa6f9551b0ff87753d76a3f1df6c
Fernando Machado Bernardino <fernandombernardino@gmail.com>
2024-05-06 17:05:31 -0300

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel'
+    ],
+    plugins: [
+	    // You can ignore this plugin if you decided to not support web version.
+	    '@babel/plugin-proposal-export-namespace-from',
+      // 🚨 Make sure to list react-native-reanimated/plugin last! 🚨
+      'react-native-reanimated/plugin'
+    ]
   };
 };
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import '../global.css';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-neutral-900">
+      <Text className="mb-6 font-mono font-bold text-xl text-white">
+	      NativeWind, is working!
+      </Text>
+      <Text className="text-6xl">
+	      🎉
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
