da756c59621d6a61225faea97e3780fc5cfc20dc
Fernando Machado Bernardino <fernandombernardino@gmail.com>
2024-05-07 09:28:10 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
       <Text className="text-6xl">
 	      🎉
       </Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   );
 }
